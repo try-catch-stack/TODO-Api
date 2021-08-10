6b0df9508d4166d5cfe637218601babf818448fd
Danish Ahmed Mirza <77742477+try-catch-stack@users.noreply.github.com>
2021-08-10 20:38:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ var path = require('path');
 var http = require('http');
 
 var oas3Tools = require('oas3-tools');
-var serverPort = 8080;
+var port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8080;
+}
 
 // swaggerRouter configuration
 var options = {
@@ -17,8 +20,8 @@ var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/open
 var app = expressAppConfig.getApp();
 
 // Initialize the Swagger middleware
-http.createServer(app).listen(serverPort, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+http.createServer(app).listen(port, function () {
+    console.log('Your server is listening on port %d (http://localhost:%d)', port, port);
+    console.log('Swagger-ui is available on http://localhost:%d/docs', port);
 });
 
